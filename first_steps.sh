10e6510612c5f9dea5245950225352b314b3d4f1
Diego Martínez Castañeda <n1mh@n1mh.org>
2016-07-18 17:14:23 +0200

    more init actions

--- first_steps.sh
+++ first_steps.sh
@@ -4,5 +4,59 @@
 # Based on Jerry Gamblin's idea:
 # http://jerrygamblin.com/2016/07/13/my-first-10-seconds-on-a-server/
 
+# Script should be executed by root. It installs and configures sudo, although.
+# This script should be obsolete as soon as I learn Ansible :)
+
 # Get the script
 # curl -sSL https://raw.githubusercontent.com/n1mh/sysadmin/master/first_steps.sh | sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+if [ $UID ] ; then
+	echo "You need to be root to run this script..."
+	exit 1
+fi
+
+# root password
+passwd
+
+# firewall
+apt-get install -y ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
+chmod 0644 ~/ufw.conf
+
+# timezone
+timedatectl set-timezone Europe/Madrid
+
+# upgrade system
+apt-get update && apt-get -y dist-upgrade
+
+# install packages
+apt-get install -y fail2ban sudo
+#apt-get install -y git unattended-upgrades
+
+# new user
+useradd diego
+passwd diego
+mkdir /home/diego
+chmod -R 700 /home/diego
+chown -R diego:diego /home/diego
+
+# configure sudo
+usermod -a -G sudo diego
+sed -i 's/%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+chmod 440 /etc/sudoers
+
+# little setup for ssh
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+echo "DebianBanner no" >> /etc/ssh/sshd_config
+echo "AllowUsers diego" >> /etc/ssh/sshd_config
+echo "MaxStartups 1" >> /etc/ssh/sshd_config
+echo "MaxAuthTries 2" >> /etc/ssh/sshd_config
+service ssh restart
+
+# successfully exit
+exit 0
