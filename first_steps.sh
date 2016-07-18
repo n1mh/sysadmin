a2ba4daf7c331ca4e4330f46cb9085c00a299926
Diego Martínez Castañeda <n1mh@n1mh.org>
2016-07-18 16:42:59 +0200

    added first steps script

---
+++ first_steps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Diego Martínez Castañeda <n1mh@n1mh.org>
+# Based on Jerry Gamblin's idea:
+# http://jerrygamblin.com/2016/07/13/my-first-10-seconds-on-a-server/
+
+# Get the script
+# curl -sSL https://raw.githubusercontent.com/n1mh/sysadmin/master/first_steps.sh | sh
