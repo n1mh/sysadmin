c9b6c3c656fde6b6653335e07a7cac19ce493ef5
Diego Martinez Castaneda <n1mh@n1mh.org>
2018-01-24 12:40:13 +0100

    added crontab explanation

--- sync_gdrive.sh
+++ sync_gdrive.sh
@@ -12,6 +12,13 @@
 #   $ rclone config
 #   $ vi ~/bin/sync_gdrive.sh
 #   $ bash ~/bin/sync_gdrive.sh
+#
+# In case you want to execute it every day (as it should be), add it to your
+# crontab. In this example it is executed every working day (Mon-Fri) at 14:30.
+#   $ crontab -l > /tmp/crontab0
+#   $ echo "30 14 * * 1-5 $HOME/bin/sync_gdrive.sh" >> /tmp/crontab0
+#   $ crontab /tmp/crontab0
+#   $ rm -f /tmp/crontab0
 
 DIRS="$HOME/documents/important $HOME/documents/hobbies"
 REMOTE_DRIVE="gdrive"
@@ -23,14 +30,19 @@ if [ `rclone lsd $REMOTE_DRIVE:$REMOTE_DIR | grep -ci 'directory not found'` = '
 fi
 
 for DIR in $DIRS ; do
-    REMOTE_DIR_NAME=`basename $DIR`
-    if [ `rclone lsd $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME | grep -ci 'directory not found'` = '1' ] ; then
-        echo "W: remote directory not found $REMOTE_DIR/$REMOTE_DIR_NAME. Creating it..."
-        rclone mkdir $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME
-    fi
 
-    echo "Sync: $DIR --> $REMOTE_DIR/$REMOTE_DIR_NAME... "
-    rclone copy --update $DIR $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME
+    if [ ! -d $DIR ] ; then
+        echo "W: local directory $DIR not found. Skipping..."
+    else
+        REMOTE_DIR_NAME=`basename $DIR`
+        if [ `rclone lsd $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME | grep -ci 'directory not found'` = '1' ] ; then
+            echo "W: remote directory not found $REMOTE_DIR/$REMOTE_DIR_NAME. Creating it..."
+            rclone mkdir $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME
+        fi
+
+        echo "Sync: $DIR --> $REMOTE_DIR/$REMOTE_DIR_NAME... "
+        rclone copy --update $DIR $REMOTE_DRIVE:$REMOTE_DIR/$REMOTE_DIR_NAME
+    fi
 done
 
 exit 0
